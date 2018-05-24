4e9a151f3b49b3dc04dab69857d84f5828d1bd62
Di Coding <dicoding@yandex.ru>
2018-05-24 15:24:50 +0300

    Step 4: add column headres to the main page, add testing links

--- src/renderers.js
+++ src/renderers.js
@@ -4,7 +4,11 @@ export const renderFeed = (feed) => {
   const rootPane = document.getElementById('v-pills-tabContent');
   const isEmptyList = rootTab.children.length === 0;
   rootTab.innerHTML = `${rootTab.innerHTML}<a class="nav-link${isEmptyList ? ' active show' : ''}" id="${guid}-tab" data-toggle="pill" href="#${guid}" role="tab" aria-controls="guid" aria-selected="false">${title}</a>`;
-  rootPane.innerHTML = `${rootPane.innerHTML}<div class="tab-pane fade${isEmptyList ? ' active show' : ''}" id="${guid}" role="tabpanel" aria-labelledby="${guid}-tab"><h3>${description}</h3><hr></div>`;
+  rootPane.innerHTML = `${rootPane.innerHTML}<div class="tab-pane fade${isEmptyList ? ' active show' : ''}" id="${guid}" role="tabpanel" aria-labelledby="${guid}-tab"><h3 class="card text-center">${description}</h3><hr></div>`;
+  if (isEmptyList) {
+    const empty = document.getElementById('emptyNotice');
+    empty.parentNode.removeChild(empty);
+  }
 };
 
 export const renderItem = (feed, item) => {
@@ -16,7 +20,10 @@ export const renderItem = (feed, item) => {
     description,
   } = item;
 
-  pane.innerHTML = `${pane.innerHTML}<div class="card" id="${guid}"><div class="card-body"><a class="card-title" href="#" data-toggle="modal" data-target="#${guid}modal" style="font-size: large;">${title}</a><div class="modal fade" id="${guid}modal" tabindex="-1" role="dialog" aria-labelledby="${guid}modalLabel" aria-hidden="true"><div class="modal-dialog modal-lg" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="${guid}modalLabel">${title}</h5><button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">${description}</div><div class="modal-footer"><button class="btn btn-primary" type="button" onclick="window.open('${link}', '_blank'); return false;">Read more</button><button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button></div></div></div></div></div></div>`;
+  pane.innerHTML = `${pane.innerHTML}<div class="card" id="${guid}"><div class="card-body"><a class="card-title" href="#" data-toggle="modal" data-target="#${guid}modal" style="font-size: large;">${title}</a></div></div>
+  <div class="modal fade" id="${guid}modal" tabindex="-1" role="dialog" aria-labelledby="${guid}modalLabel" aria-hidden="true"><div class="modal-dialog modal-lg" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="${guid}modalLabel">${title}</h5><button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">${description}</div><div class="modal-footer"><button class="btn btn-primary" type="button" onclick="window.open('${link}', '_blank'); return false;">Read more</button><button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button></div></div></div></div>`;
+
+
 };
 
 export const renderError = (err) => {
