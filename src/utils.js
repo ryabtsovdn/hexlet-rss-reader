43e6c1b54a10ffcbea48401f0c92caacc96d4273
Di Coding <dicoding@yandex.ru>
2018-05-25 17:26:22 +0300

    fix images overlapping div problem

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,8 @@ export const cleanDescription = str =>
     .replace(/&amp;/g, '&')
     .replace(/&apos;/g, "'")
     .replace(/&quot;/g, '"')
-    .replace(/<\/img>/g, '');
+    .replace(/<\/img>/g, '')
+    .replace(/<img(.*)>/g, '<img class="mw-100" $1>');
 
 export const getProp = (element, prop) =>
   replaceCDATA(element.querySelector(prop).innerHTML);
