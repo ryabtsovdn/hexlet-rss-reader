8c7879ec5894f1d88abb767b5a0f1c8b4fef5b38
Di Coding <dicoding@yandex.ru>
2018-05-26 10:41:09 +0300

    fix travis build problem #4 (temporary)

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 
 const devMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
-  resolve: {
-    alias: {
-      assets: path.join(__dirname, '/src/assets/'),
-    },
-  },
   module: {
     rules: [
       {
